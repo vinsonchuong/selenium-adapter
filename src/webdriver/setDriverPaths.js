f9c200c8c0fa57299605f5450fc11ec1eab49269
Vinson Chuong <vinsonchuong@gmail.com>
2017-06-02 22:18:41 -0700

    Prevent driver paths from ending up on the bottom of the search path

--- src/webdriver/setDriverPaths.js
+++ src/webdriver/setDriverPaths.js
@@ -1,12 +1,17 @@
 /* @flow */
 import * as path from 'path'
-import {path as chromedriverPath} from 'chromedriver'
-import {path as geckodriverPath} from 'geckodriver'
 import {path as phantomPath} from 'phantomjs-prebuilt'
 import once from 'selenium-adapter/src/fntools/once'
+import getEntries from 'selenium-adapter/src/pathtools/getEntries'
+import setEntries from 'selenium-adapter/src/pathtools/setEntries'
 import prepend from 'selenium-adapter/src/pathtools/prepend'
 
 export default once((): void => {
+  const originalEntries = getEntries()
+  const chromedriverPath = require('chromedriver').path
+  const geckodriverPath = require('geckodriver').path
+  setEntries(originalEntries)
+
   prepend(path.dirname(chromedriverPath))
   prepend(path.dirname(geckodriverPath))
   prepend(path.dirname(phantomPath))
